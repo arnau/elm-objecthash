bddb838fa7fe6ad97cbe295148a5920996b105d6
Arnau Siches <asiches@gmail.com>
2018-08-09 08:17:17 +0100

    docs: Document the Objecthash module
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/ObjectHash.elm
+++ src/ObjectHash.elm
@@ -13,13 +13,24 @@ import Objecthash.Json as Json
 import Objecthash.Value exposing (Value(..))
 
 
-{-| -}
+{-| Hash an `Objecthash.Value` and return it as a hexadecimal string
+representation.
+
+If you need more control over the hash, `Objecthash.Hash` offers a range of
+functions to operate on elm values and lists of bytes.
+
+-}
 objecthash : Value -> String
 objecthash value =
     (toHex << bytes) value
 
 
-{-| Common JSON
+{-| Hash a Common JSON blob and return it as a hexadecimal string
+representation.
+
+If you need more control over the JSON decoder, check the `Objecthash.Json`
+module.
+
 -}
 fromJson : String -> Result String String
 fromJson input =
